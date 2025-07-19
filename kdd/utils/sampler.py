945cbafce69cf68f18e028cd8566c151da29f34a
brkim <fried4chicken@kentech.ac.kr>
2025-07-20 00:53:27 +0900

    update

--- kdd/utils/sampler.py
+++ kdd/utils/sampler.py
@@ -1,5 +1,3 @@
-# Need be fixed.
-
 from environment import *
 logger = logging.getLogger(name = __name__)
 
@@ -10,10 +8,10 @@ class Sampler:
     def __repr__(self):
         return 'sampler'
 
-    def sample(self, A, size, axis = 0, replace = False):
+    def sample(self, A, size_in, axis = 0, replace = False):
         if not isinstance(A, np.ndarray):
             raise TypeError('The array should be a \'numpy.ndarray\'.')
-        if not isinstance(size, int):
+        if not isinstance(size_in, int):
             raise TypeError('The sample size should be an integer.')
         if not isinstance(axis, int):
             raise TypeError('\'axis\' should be an integer.')
@@ -21,13 +19,16 @@ class Sampler:
             raise TypeError('\'replace\' should be boolean.')
         if A.ndim < 1:
             raise ValueError('The array should be at least 1-dimensional.')
-        if size < 1:
+        if size_in < 1:
             raise ValueError('The sample size must be positive.')
         if not -A.ndim <= axis < A.ndim:
             raise ValueError('Not a valid axis.')
-        if size > A.shape[axis]:
+
+        if size_in > A.shape[axis]:
             logger.warning('The sample size is bigger than the sampled.')
             size = A.shape[axis]
+        else:
+            size = copy(size_in)
 
         index = np.random.choice(
             A.shape[axis],
