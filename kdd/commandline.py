74fa66315557fd81c62a6def74b3655d0de3b835
brkim <fried4chicken@kentech.ac.kr>
2025-08-01 14:11:53 +0900

    update

--- kdd/commandline.py
+++ kdd/commandline.py
@@ -4,7 +4,7 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--resplit', help = 'whether to merge the train and test sets and resplit randomly, retaining the attack type distribution.', default = 'False')
-parser.add_argument('--qthreshold', help = 'the quantile threshold above which the reconstruction loss is deemed as anomalous', default = '0.99')
+parser.add_argument('--qthreshold', help = 'the quantile threshold above which the reconstruction loss is deemed anomalous', default = '0.99')
 parser.add_argument('--log', help = 'logging level', default = 'INFO')
 
 args = parser.parse_args()
