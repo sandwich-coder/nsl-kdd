2b444e8e7b0f8c30d83eff685098fc812c352c24
brkim <fried4chicken@kentech.ac.kr>
2025-08-12 18:17:00 +0900

    update

--- temp/housing.py
+++ temp/housing.py
@@ -21,14 +21,14 @@ result['short']['fail'] = - 6 * 10    #out two days after the announcement
 #check in 1 month first and extend on success
 result['long1']['success'] = - 90 * 1 - 75 * 3
 result['long1']['fail'] = - 90 * 1    #out in a month
-temp = - 7 * 10 - 30    #out two days after the announcement
+temp = - 6 * 10 - 30    #out two days after the announcement | small discount applied to unintended extensions as well
 if result['long1']['fail'] < temp:
     result['long1']['fail'] = temp
 
 #check in 4 months and remain on success
 result['long2']['success'] = - 70 * 4
 result['long2']['fail'] = - 90 * 1 - 30    #out in a month
-temp = - 7 * 10 - 30    #out two days after the announcement
+temp = - 6 * 10 - 30    #out two days after the announcement
 if result['long2']['fail'] < temp:
     result['long2']['fail'] = temp
 
@@ -58,12 +58,12 @@ for l in chance:
     expected['long2'] = round(result['long2']['success'] * l + result['long2']['fail'] * (1 - l), ndigits = 1)
 
     #most efficient option
-    most_saving = max(*expected.values())
-    efficient = list(expected.values()).index(most_saving)
-    efficient = list(expected.keys())[efficient]
+    cheap = max(*expected.values())
+    cheap = list(expected.values()).index(cheap)
+    cheap = list(expected.keys())[cheap]
 
-    print('{:>5}% || {:>6} | {:>6} | {:>6}   -> efficient choice :  {}'.format(
+    print('{:>5}% || {:>6} | {:>6} | {:>6}   -> cheapest choice :  {}'.format(
         round(l * 100, ndigits = 1),
         expected['short'], expected['long1'], expected['long2'],
-        efficient,
+        cheap,
         ))
