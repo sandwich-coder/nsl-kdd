6540f89abe42595d571c1ea6a5eda6b6cab5b825
brkim <fried4chicken@kentech.ac.kr>
2025-08-18 17:33:31 +0900

    update

--- misc/pandas_/category_.py
+++ misc/pandas_/category_.py
@@ -7,13 +7,22 @@ df = pd.DataFrame({
 df = df.astype('category', copy = False)
 
 
-first = df.iloc[:3, 0].copy()    #first three
-second = df.iloc[3:, 0].copy()    #last three
+first = df.iloc[:3, :].copy()    #first three
+second = df.iloc[3:, :].copy()    #last three
 
 first_onehot = pd.get_dummies(first, columns = ['letter'])
 second_onehot = pd.get_dummies(second, columns = ['letter'])
 
 
+# The categories that don't even exist are listed by the 'value_counts', with the counts 0.
+# Furthermore, the 'get_dummies' "onehots" based not on what actually are but rather on what are listed in the category dtype.
+# What actually are affect not the onehot behavior but the initial conversion to the catogory dtype.
+
+print(first['letter'].value_counts())
+print('')
 print(first_onehot)
+print('\n')
+
+print(second['letter'].value_counts())
 print('')
 print(second_onehot)
