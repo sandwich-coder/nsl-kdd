ec395a15988669043ce97d54b28697db546eba6e
brkim <fried4chicken@kentech.ac.kr>
2025-08-23 14:45:25 +0900

    update

--- experiment/kdd.py
+++ experiment/kdd.py
@@ -10,23 +10,26 @@ import logging
 logger = logging.getLogger(name = 'experiment')
 import numpy as np
 from scipy import linalg as la
+import matplotlib as mpl
+from matplotlib import pyplot as pp
+mpl.rcParams.update({
+    'figure.figsize':[10, 10],
+    'figure.edgecolor':'none',
+    'figure.facecolor':'none',
+    'axes.spines.top':False,
+    'axes.spines.right':False,
+    'axes.edgecolor':'none',
+    'axes.facecolor':'none',
+    'lines.markersize':1,
+    'lines.linewidth':0.5,
+    'legend.edgecolor':'none',
+    'legend.facecolor':'none',
+    })
 
 import pandas as pd
 import torch
 from torch import optim, nn
 import xgboost as xgb
-import matplotlib as mpl
-from matplotlib import pyplot as pp
-mpl.rcParams.update({
-    'figure.figsize':(10, 10),
-    'axes.titlesize':'medium',
-    'axes.labelsize':'x-small',
-    'xtick.labelsize':'xx-small',
-    'ytick.labelsize':'xx-small',
-    'legend.fontsize':'x-small',
-    'lines.markersize':1,
-    'lines.linewidth':0.5,
-    })
 from rich.console import Console
 from rich.text import Text
 from rich.table import Table
